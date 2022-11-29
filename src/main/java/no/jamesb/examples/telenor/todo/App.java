8942fb5bcf4dc0ae3b2fc3f22b57fbb2c9963325
James Bradlee <post@jamesb.no>
2022-11-29 22:06:39 +0000

    fmt

--- src/main/java/no/jamesb/examples/telenor/todo/App.java
+++ src/main/java/no/jamesb/examples/telenor/todo/App.java
@@ -1,7 +1,8 @@
 package no.jamesb.examples.telenor.todo;
 
+/** The root Application object for the project. */
 public class App {
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
 }
