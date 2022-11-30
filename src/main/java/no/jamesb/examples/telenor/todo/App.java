2545d0182c827aff5dae1cf86d5d6e94141a061a
James Bradlee <post@jamesb.no>
2022-11-30 03:09:13 +0000

    Configure the project for Spring Framework

--- src/main/java/no/jamesb/examples/telenor/todo/App.java
+++ src/main/java/no/jamesb/examples/telenor/todo/App.java
@@ -1,8 +1,12 @@
 package no.jamesb.examples.telenor.todo;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /** The root Application object for the project. */
+@SpringBootApplication
 public class App {
   public static void main(String[] args) {
-    System.out.println("Hello World");
+    SpringApplication.run(App.class, args);
   }
 }
---
+++ src/main/java/no/jamesb/examples/telenor/todo/controllers/HelloController.java
@@ -0,0 +1,16 @@
+package no.jamesb.examples.telenor.todo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Controls the Greeter API.
+ */
+@RestController
+public class HelloController {
+  @GetMapping("/{name}")
+  public String index(@PathVariable("name") String name) {
+    return "Hello, " + name + "!";
+  }
+}
